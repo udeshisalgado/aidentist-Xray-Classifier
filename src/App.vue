6ab945b23e0707f3219f3ab563be3da3477917ba
Udeshi Salgado <udeshisalgado1998@gmail.com>
2023-06-16 03:00:30 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app>
+    <!-- <v-app-bar
+      color="primary"
+      dark
+
+    >
+    <img src="./assets/AI DENTIST LOGO.jpg" alt="" srcset="" width="80" height="80">
+    <p class="text-h6 text-md-h5 text-lg-h4 ml-4"> Aidentist: X-ray Classifier</p>
+    
+    </v-app-bar> -->
+    <v-card elavation="2" color="primary" class="pa-2" height="90" dark>
+      <div class="mt-4 ml-4">
+      <v-row>
+        <img src="./assets/AI DENTIST LOGO.jpg" alt="" srcset="" width="60" height="60" class="mb-0">
+        <p class="text-h6 text-md-h5 text-lg-h4 ml-4"> Aidentist: X-ray Classifier</p>
+      </v-row>
+      <p class="ml-15" style="margin-top: -20px; margin-left: 30px;">Tooth anomaly detector for X-ray dental images.</p>
+    </div>
+    </v-card>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //hhh
+  }),
+};
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,101 @@
+<template>
+  <div>
+    <!-- <v-layout>
+      <v-main>
+        <v-container fluid> -->
+          <v-row class="mx-4 mt-12">
+            <v-col cols="4">
+              <input ref="fileInput" type="file" @input="pickFile" />
+            </v-col>
+            <v-col cols="4">
+              <div>
+                <v-card width="200" height="200" variant="outlined">
+                  <v-img
+                  width="200"
+                  height="200"
+                  aspect-ratio="4/3"
+                  cover
+                  :src="previewImage"
+                ></v-img>
+                </v-card>
+                
+              </div>
+            </v-col>
+            <v-col cols="4">
+              <p><span class="font-weight-black">Label:</span> value</p>
+                <h3 class="mb-5 text-decoration-underline">Prediction Result</h3>
+                <p><span class="font-weight-black">Caries:</span> value</p>
+                <p><span class="font-weight-black">Caries under filling:</span> value</p>
+                <p><span class="font-weight-black">Bone loss:</span> value</p>
+                <p><span class="font-weight-black">Impaction:</span> value</p>
+            </v-col>
+          </v-row>
+        <!-- </v-container>
+      </v-main>
+    </v-layout> -->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialog: false,
+      file: null,
+      imageUrl: "",
+      currentImage: undefined,
+      // previewImage: undefined,
+      previewImage: null,
+      imageSelected: false,
+    };
+  },
+  //   watch: {
+  //   file(newFile) {
+  //     if (newFile) {
+  //       this.previewImage(newFile);
+  //     }
+  //   },
+  // },
+  methods: {
+    // previewImage(file) {
+    //   const reader = new FileReader();
+    //   reader.onload = () => {
+    //     this.imageUrl = reader.result;
+    //   };
+    //   reader.readAsDataURL(file);
+    // },
+    // selectImage(image) {
+    //   this.currentImage = image;
+    //   this.previewImage = URL.createObjectURL(this.currentImage);
+    //   this.progress = 0;
+    //   this.message = "";
+    // },
+    selectImage () {
+          this.$refs.fileInput.click()
+      },
+      pickFile () {
+        let input = this.$refs.fileInput
+        let file = input.files
+        if (file && file[0]) {
+          let reader = new FileReader
+          reader.onload = e => {
+            this.previewImage = e.target.result
+          }
+          reader.readAsDataURL(file[0])
+          this.$emit('input', file[0])
+        }
+      }
+  },
+};
+</script>
+<style scoped lang="scss">
+.imagePreviewWrapper {
+    width: 250px;
+    height: 250px;
+    display: block;
+    cursor: pointer;
+    margin: 0 auto 30px;
+    background-size: cover;
+    background-position: center center;
+}
+</style>
